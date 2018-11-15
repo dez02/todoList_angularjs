7ce657ccd3405c487924020014388c689709eafb
Désirée <deutodesiree@gmail.com>
2018-11-16 00:38:22 +0100

    modifs todoModel

--- api_todos/models/todoModel.js
+++ api_todos/models/todoModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const todoSchema = new Schema({
-  name: { type: String, require: true }
+  name: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Todo', todoSchema);
